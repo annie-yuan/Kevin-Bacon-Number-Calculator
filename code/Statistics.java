c414439f93af4343739af930c7dbbe57671429f3
annie-yuan <70941019+annie-yuan@users.noreply.github.com>
2023-03-02 15:45:56 -0500

    Update Statistics.java

--- code/Statistics.java
+++ code/Statistics.java
@@ -3,30 +3,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-/**
- * This class is a helper class for calculating statistics
- * based on a list of integer values. In both Question 1 and
- * Question 2, you will need to calculate the min, max,
- * mean, Q1, Q2, and Q3 of a list of integers, as well as
- * count the frequency of each integer.
- *
- * IMPORTANT: This class is not yet complete, so must be
- * completed by you.
- *
- * To accomplish this, you should fill in the missing parts
- * of this code. Project 3 builds upon Projects 1 and 2
- * here! Finding the top N foods by calorie count is similar
- * to finding the first second and third quartiles. I
- * recommend you consider using a min heap (via the
- * PriorityQueue class) to complete this code. However, you
- * may use whatever means you prefer (sorting, your solution
- * to Project 1, etc.).
- *
- * @author Mark Hancock
- * @author <Annie Yuan> - you are going to author one of the
- *         methods
- *
- */
+//This class is a helper class for calculating statistics  based on a list of integer values. 
+
 public class Statistics {
     private HashMap<Integer, Integer> counts = new HashMap<>();
     private int min;//get minimum
@@ -36,37 +14,22 @@ public class Statistics {
     private double q2;//get second quartile
     private double q3;//third quartile
 
-    /**
-     * Constructs a new Statistics object based on a list of
-     * integers. This class doesn't store the integers, but
-     * instead calculates the min, max, Q1-Q3, and counts
-     * the number of occurrences of each value.
-     *
-     * @param list the list of integers to collect
-     *             statistics from.
-     */
+	//Constructs a new Statistics object based on a list of integers. 
+	//This class doesn't store the integers, but instead calculates the min, max, Q1-Q3, 
+	//and counts the number of occurrences of each value.
+	
+	//@param list the list of integers to collect statistics from.
+
     public Statistics(List<Integer> list) {
         calculateFromList(list);
     }
 
-    /**
-     * This method calculates the min, max, mean, Q1, Q2,
-     * and Q3 values from a list of integers, and also
-     * counts the frequency (number of occurrences) of each
-     * repeated value in the list.
-     *
-     * @param list a list of unsorted integers, with
-     *             possible repeated values.
-     */
+	//This method calculates the min, max, mean from a list of integers, and also
+	//counts the frequency (number of occurrences) of each repeated value in the list.
+
+	//@param list a list of unsorted integers, with possible repeated values.
+
     private void calculateFromList(List<Integer> list) {
-        /*
-         * TODO: complete this method!
-         * 
-         * By the end of this method, you should have
-         * calculated the min, max, mean, Q1, Q2, and Q3
-         * values, as well as populated the counts HashMap
-         * with the counts of each value.
-         */
     	
     	 min = list.get(0);//initialize the first value in the list as min
          max = list.get(0);//initialize max
@@ -116,81 +79,60 @@ public class Statistics {
     	 }
     }
 
-    /**
-     * Returns a list of ordered unique values based on the
-     * original list with duplicates.
-     *
-     * @return a list of ordered unique values based on the
-     *         original list with duplicates
-     */
+    // Returns a list of ordered unique values based on the original list with duplicates.
+   
     public List<Integer> getSortedUniqueKeys() {
         ArrayList<Integer> keys = new ArrayList<>(counts.keySet());
         Collections.sort(keys);
         return keys;
     }
 
-    /**
-     * Returns the number of occurrences of a specific
-     * value.
-     *
-     * @param value the value to check for occurrences
-     * @return the number of occurrences
-     */
+    // Returns the number of occurrences of a specific value.
+    // @param value the value to check for occurrences
+    // @return the number of occurrences
+
     public int getCountOf(int value) {
         return counts.get(value);
     }
 
-    /**
-     * Returns the minimum value from the list.
-     *
-     * @return the minimum value from the list
-     */
+    //Returns the minimum value from the list.
+    //@return the minimum value from the list
+  
     public int getMin() {
         return min;
     }
 
-    /**
-     * Returns the maximum value from the list.
-     *
-     * @return the maximum value from the list
-     */
+    //Returns the maximum value from the list.
+    //@return the maximum value from the list
+     
     public int getMax() {
         return max;
     }
 
-    /**
-     * Returns the mean value from the list.
-     *
-     * @return the mean value from the list
-     */
+    //Returns the mean value from the list.
+    //@return the mean value from the list
+     
     public double getMean() {
         return mean;
     }
 
-    /**
-     * Returns the first quartile value from the list.
-     *
-     * @return the first quartile value from the list
-     */
+    //Returns the first quartile value from the list.
+    //@return the first quartile value from the list
+     
     public double getQ1() {
         return q1;
     }
 
-    /**
-     * Returns the second quartile (a.k.a. median) value
-     * from the list.
-     *
-     * @return the first quartile value from the list
-     */
+    //Returns the second quartile (a.k.a. median) value from the list.
+    // @return the first quartile value from the list
+     
     public double getQ2() {
         return q2;
     }
 
-    /**
-     * Returns the third quartile value from the list.
-     *
-     * @return the third quartile value from the list
-     */
+    // Returns the third quartile value from the list.
+    //@return the third quartile value from the list
+     
     public double getQ3() {
         return q3;
     }
